e3e22b8af7f69f99a4e4202e26937568944b4838
German Labra <germanalbertolabra@gmail.com>
2018-12-06 18:55:34 -0300

    checkbox

--- src/app/modules/login/login-form.model.ts
+++ src/app/modules/login/login-form.model.ts
@@ -1,11 +1,13 @@
 export class LoginFormModel {
   email: string;
   password: string;
+  rememberMe: boolean;
 
   constructor(values: {
-    [key: string]: string
+    [key: string]: any
   } = {}) {
     this.email = values.email;
     this.password = values.password;
+    this.rememberMe = values.rememberMe;
   }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterModule } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { CoreModule } from '../core';
 
@@ -30,6 +31,7 @@ import { LoginComponent } from './login.component';
     MatIconModule,
     MatInputModule,
     MatSnackBarModule,
+    MatCheckboxModule
   ],
   exports: [
     LoginComponent

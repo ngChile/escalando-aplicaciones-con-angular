1e693ef7f437d34e602ac184e4b7b713bcc4016f
Iran Sosa <iran_sosa_7@hotmail.com>
2018-12-07 10:20:20 -0300

    Checkbox en el login

--- src/app/modules/login/login-form.model.ts
+++ src/app/modules/login/login-form.model.ts
@@ -2,12 +2,14 @@ export class LoginFormModel {
   email: string;
   password: string;
   group: string;
+  rememberMe: boolean;
 
   constructor(values: {
-    [key: string]: string //any
+    [key: string]: any //any
   } = {}) {
     this.email = values.email;
     this.password = values.password;
     this.group = values.group;
+    this.rememberMe = values.rememberMe;
   }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { CoreModule } from '../core';
 import { LoginComponent } from './login.component';
@@ -27,6 +28,7 @@ import { AuthGuard } from './auth.guard';
     HttpClientModule,
     CoreModule,
     MatSelectModule,
+    MatCheckboxModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,

f8546be8764f17f20e2dcdb924a4fd19f8ffb1bf
Jose Lara <jlarauk@gmail.com>
2018-12-07 13:01:43 -0300

    Carga Select desde BackEnd

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -7,6 +7,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from './login.service';
 
 import { LoginFormModel } from './login-form.model';
+import { GroupService } from './group.service';
 
 @Component({
   selector: 'app-login',
@@ -18,12 +19,14 @@ export class LoginComponent implements OnInit {
 
   formModel: LoginFormModel;
   isLoading: boolean;
+  groups: [];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private snackBar: MatSnackBar,
     private loginService: LoginService,
+    private groupService: GroupService
   ) {
     this.formModel = new LoginFormModel({
       email: this.route.snapshot.queryParams.email,
@@ -32,7 +35,13 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.groupService
+        .getGroups()
+        .then((response: any) => {
+          this.groups = response.list;
+        });
+  }
   submit() {
     if (this.loginForm.valid) {
       this.isLoading = true;

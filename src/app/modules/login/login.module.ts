39af6bc479ad7a35e588b0a0a348610544bdf160
Miguel Munoz <mmunozl88@gmail.com>
2018-12-10 11:03:47 -0300

    se arreglo modulo de Login

---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CoreModule } from '../core';
+import { LoginService } from './login.service';
+import { AuthGuard } from './auth.guard';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [ CommonModule,
+  FormsModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatCardModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  CoreModule,
+  HttpClientModule,
+  MatButtonModule,
+  MatInputModule,
+ ],
+  providers: [LoginService, AuthGuard]
+})
+export class LoginModule {}

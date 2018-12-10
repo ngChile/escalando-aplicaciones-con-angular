201f9de717836b2dab82d43858adad804b3a147a
Moises Cadima <moises.cadima@gmail.com>
2018-12-10 11:04:28 -0300

    Inyectando dependencias y servicios.

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,21 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-
-import { CoreModule } from '../core';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LoginService } from './login.service';
 import { AuthGuard } from './auth.guard';
-
-//CheckBox
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { CoreModule } from '../core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -28,15 +26,16 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   imports: [
     CommonModule,
     FormsModule,
-    HttpClientModule,
-    CoreModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatCardModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCardModule,
     MatCheckboxModule,
+    CoreModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class LoginModule { }

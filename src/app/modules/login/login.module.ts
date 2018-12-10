4010ce24183bb51a6f42963fce258673aea2f308
Ismael Vicencio <imvicencio@live.cl>
2018-12-10 11:04:28 -0300

    Creacion de modulo login

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,38 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { CoreModule } from '../core';
-import { LoginComponent } from './login.component';
 import { LoginService } from './login.service';
 import { AuthGuard } from './auth.guard';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  providers: [
-    LoginService,
-    AuthGuard
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    HttpClientModule,
-    CoreModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatCheckboxModule,
-  ]
+  declarations: [LoginComponent],
+  providers: [LoginService,AuthGuard],
+  imports: [ CommonModule, 
+  FormsModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatButtonModule,
+  MatInputModule,
+  CoreModule,
+  HttpClientModule
+],
+  
 })
-export class LoginModule { }
+export class LoginModule {}

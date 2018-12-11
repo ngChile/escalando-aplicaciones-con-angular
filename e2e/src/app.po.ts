93d9b2df9e9393fdd0ad90ee717de17ad8474dc1
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2018-12-11 01:14:27 -0300

    fix e2e test and adding specs for login e2e test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -3,7 +3,8 @@ import { browser, by, element } from 'protractor';
 export class AppPage {
   selectors = {
     'title' : 'app-root h1'
-  }
+  };
+
   navigateTo() {
     return browser.get('/');
   }

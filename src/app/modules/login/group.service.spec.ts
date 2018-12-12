ae70e5012e1a4201996b2b28adac02bc60b37375
hakendo <moises.cadima@gmail.com>
2018-12-12 11:19:41 -0300

    Integración de pruebas unitarias.

--- src/app/modules/login/group.service.spec.ts
+++ src/app/modules/login/group.service.spec.ts
@@ -1,20 +1,62 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
 import { GroupService } from './group.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
 
-describe('GroupService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule
-    ],
-    providers: [
-      GroupService
-    ]
-  }));
+class HttpClientMock {
 
-  it('should be created', () => {
-    const service: GroupService = TestBed.get(GroupService);
-    expect(service).toBeTruthy();
+  get = jasmine.createSpy();
+
+}
+
+fdescribe('Group Service', () => {
+
+  let service: GroupService;
+  let httpClientMock: HttpClientMock;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        {
+          provide: HttpClient,
+          useClass: HttpClientMock
+        },
+        GroupService
+      ]
+    });
+
+    service = TestBed.get(GroupService);
+    httpClientMock = TestBed.get(HttpClient);
+
   });
+
+  it('should be created an instance', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('test', () => {
+
+    httpClientMock.get.and.returnValue({
+      toPromise: () => null
+    });
+
+    const result = service.getGroups();
+
+    expect(httpClientMock.get).toHaveBeenCalledWith(environment.endpoint.groups);
+    expect(result).toBe(null);
+
+  });
+
+  it('should be set an arry of gruops and expose a getter', () =>{
+    const list = [1, 2, 3];
+
+    service.setGroups(list);
+
+    expect(service.getStoredGroups()).toEqual(list);
+  })
+
 });

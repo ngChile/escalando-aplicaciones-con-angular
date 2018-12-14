ab5a5e358533f6a3b31728276891bb40c263b245
Yerko Palma <yerko.palma@usach.cl>
2018-12-14 12:50:09 -0300

    reactive driven register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
-import { finalize } from 'rxjs/operators';
-import { Validators } from '../../modules/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { RegisterService } from './register.service';
 
 @Component({
@@ -14,59 +9,32 @@ import { RegisterService } from './register.service';
 })
 export class RegisterComponent implements OnInit {
 
-  isLoading: boolean;
-  form: FormGroup;
+  form = new FormGroup({
+    fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+  isLoading = false;
 
   constructor(
-    private router: Router,
-    private snackBar: MatSnackBar,
-    private registerService: RegisterService,
-  ) {
-    this.form = new FormGroup({
-      fullName: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(100)
-      ]),
-      email: new FormControl('', [
-        Validators.required,
-        Validators.email,
-        Validators.minLength(3),
-        Validators.maxLength(100)
-      ]),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(50)
-      ]),
-      rePassword: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(50),
-      ], [
-        Validators.equalsTo('password'),
-      ]),
-    });
-  }
+    private registerService: RegisterService
+  ) { }
 
   ngOnInit() {
   }
 
-  submit() {
+  onSubmit() {
     if (this.form.valid) {
       this.isLoading = true;
-      this.registerService.register(this.form.value).pipe(
-        finalize(() => this.isLoading = false),
-      ).subscribe(user => {
-        this.router.navigate(['/login'], {
-          queryParams: {
-            email: user.email,
-          },
+      this.registerService
+        .register(this.form.value)
+        .subscribe(response => {
+          this.isLoading = false;
+          alert(response);
+        }, reason => {
+          this.isLoading = false;
+          alert(JSON.stringify(reason, null, 2));
         });
-      }, errorResponse => {
-        this.snackBar.open(errorResponse.error.message, null, { duration: 5000 });
-      });
     }
   }
-
 }
--- src/app/pages/register/register.service.ts
+++ src/app/pages/register/register.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
-
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { retry, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  register(user: User): Observable<User> {
-    return this.http.post<User>(environment.endpoint.register, user).pipe(
-      retry(2),
-    );
+  register(user): Observable<string> {
+    return this.http
+      .post<UserResponse>(environment.endpoint.register, user)
+      .pipe(
+        retry(2),
+        map(response => response.fullName)
+      );
   }
-
 }
 
-export interface User {
+interface UserResponse {
   fullName: string;
   email: string;
-  password: string;
+  id: number;
 }

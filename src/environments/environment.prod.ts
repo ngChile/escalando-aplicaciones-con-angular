7a36304b910dae0f25dabf27f2fee9e3cb5d5b69
Darío Esteban Verdugo Leiva <darioesteban.verdugo@gmail.com>
2018-12-18 12:56:04 -0300

    Guards

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   endpoint: {
-    auth: '/auth-service/v1/login',
-    logout: '/auth-service/v1/logout',
-    register: '/auth-service/v1/register',
+    auth: 'http://localhost:8882/auth-service/v1/login',
+    logout: 'http://localhost:8882/auth-service/v1/logout',
+    register: 'http://localhost:8882/auth-service/v1/register',
+    groups: 'http://localhost:8882/auth-service/v1/groups',
   }
 };

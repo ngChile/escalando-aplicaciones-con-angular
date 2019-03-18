07ff993d744640f628656d4098e58c19fadc5371
sbstn_jmnz <sbstn.jmnz@yahoo.com>
2019-03-17 23:31:54 -0300

    BDD excercise one

--- e2e/support/step_definitions/login_steps.js
+++ e2e/support/step_definitions/login_steps.js
@@ -1,11 +1,20 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-before(() => {
+beforeEach(() => {
     cy.server();
     cy.fixture('groups.json').as('groupsJSON');
     cy.route('GET', '/auth-service/v1/groups', '@groupsJSON');
 });
 
-Given('ingreso a la pagina de autenticación', () => {
+Given('ingreso a la página de autenticación', () => {
     cy.visit('/');
 });
+
+When('hago clic en el enlace de postulación a la beca', () => {
+    cy.get('.login-page__scholarship-link').click()
+})
+
+Then('soy redirigido a la página de postulación', () => {
+    cy.url().should('contains','/scholarship-form');    
+})
+
---
+++ e2e/support/step_definitions/scholarship-form_steps.js
@@ -0,0 +1,29 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+
+Given('ingreso a la página de postulación', () => {
+    cy.visit('/scholarship-form');
+});
+
+When('escribo un nombre en el input de nombre', () => {
+    cy.get('.scholarship-form-page__name').type('Mi nombre')
+})
+
+When('escribo un nombre en el input de email', () => {
+    cy.get('.scholarship-form-page__email').type('MiNombre@test.org')
+})
+
+When('escribo una razon en el text area de motivo', () => {
+    cy.get('.scholarship-form-page__reason').type('Me gusta mucho el curso')
+})
+
+When('hago clic en el botón postular', () => {
+    cy.get('.scholarship-form-page__submit').click()
+})
+
+Then('debería ver el mensaje de postulación enviado', () =>{
+    cy.get('.scholarship-form-page__result-text').invoke('text').should('contains', 'Mensaje enviado con éxito')
+})
+
+
+

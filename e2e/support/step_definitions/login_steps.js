d2dc7d8bd4df03d2e416a615f29e3123cbf2f502
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2019-03-21 00:42:44 -0300

    adding support for login to login steps in e2e tests

--- e2e/support/step_definitions/login_steps.js
+++ e2e/support/step_definitions/login_steps.js
@@ -3,7 +3,9 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 beforeEach(() => {
     cy.server();
     cy.fixture('groups.json').as('groupsJSON');
+    cy.fixture('user.json').as('userJSON');
     cy.route('GET', '/auth-service/v1/groups', '@groupsJSON');
+    cy.route('POST', '/auth-service/v1/login', '@userJSON')
 });
 
 Given('ingreso a la página de autenticación', () => {

9c435892e1bfb1bb7dd474ac432f7782f7a3194e
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2019-10-24 09:07:33 -0300

    ci (build): attach workspace variables in build job

--- index-html-transform.js
+++ index-html-transform.js
@@ -1,5 +1,5 @@
 const { version } = require("./package.json");
 
 module.exports = (targetOptions, indexHtml) => {
-    return indexHtml.replace('APP_VERSION', version);
+    return indexHtml.replace('APP_VERSION', process.env.PACKAGE_VERSION_TARGET || version);
 }
